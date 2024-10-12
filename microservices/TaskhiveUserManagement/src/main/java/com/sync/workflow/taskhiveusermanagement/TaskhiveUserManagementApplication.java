3e6e8be04b4ac0374b936be7076aeffaed42aca9
Saurabh Mishra <69413598+eternal-harsh@users.noreply.github.com>
2024-10-12 12:56:25 +0530

    Adding shell of TaskHive User Management Service

---
+++ microservices/TaskhiveUserManagement/src/main/java/com/sync/workflow/taskhiveusermanagement/TaskhiveUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sync.workflow.taskhiveusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhiveUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskhiveUserManagementApplication.class, args);
+    }
+
+}
---
+++ microservices/TaskhiveUserManagement/src/test/java/com/sync/workflow/taskhiveusermanagement/TaskhiveUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sync.workflow.taskhiveusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhiveUserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
